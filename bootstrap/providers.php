98158c75b53a0b1d47287ba396f9cb3b4b72f0b6
Phan Văn Hoàng <hoangyh2712@gmail.com>
2026-04-08 09:29:26 +0700

    Fix service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Modules\Auth\Providers\AuthServiceProvider;
+use App\Modules\User\Providers\UserServiceProvider;
 use App\Providers\AppServiceProvider;
 use Laravel\Socialite\SocialiteServiceProvider;
 
@@ -9,6 +10,7 @@ return [
     L5Swagger\L5SwaggerServiceProvider::class,
     AppServiceProvider::class,
     AuthServiceProvider::class,
+    UserServiceProvider::class,
     SocialiteServiceProvider::class,
 
 ];
