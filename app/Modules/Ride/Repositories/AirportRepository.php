a8f705fd51bb5799722580d59df2e0b516ef641a
Phan Văn Hoàng <hoangyh2712@gmail.com>
2026-04-23 18:00:16 +0700

    Update api ariport

---
+++ app/Modules/Ride/Repositories/AirportRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Ride\Repositories;
+
+use App\Core\Repository\BaseRepository;
+use App\Modules\Ride\Interfaces\AirportRepositoryInterface;
+use App\Modules\Ride\Model\Airport;
+use Illuminate\Support\Collection;
+
+final class AirportRepository extends BaseRepository implements AirportRepositoryInterface
+{
+    public function getModel(): string
+    {
+        return Airport::class;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getActiveAirports(): Collection
+    {
+        return $this->model
+            ->where('is_active', true)
+            ->orderBy('name')
+            ->get();
+    }
+}
